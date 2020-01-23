d9d08255d1a19830d570d439879cba6e96ef15d0
Julio Treviño Páez <julio.tpaez@gmail.com>
2020-01-23 20:18:59 +0100

    Update page title and favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ const config: Configuration = {
      ** Headers of the page
      */
     head: {
-        title: process.env.npm_package_name || "",
+        title: "Julio Treviño Páez Personal Web" || "",
         meta: [{charset: "utf-8"}, {
             name: "viewport",
             content: "width=device-width, initial-scale=1"
